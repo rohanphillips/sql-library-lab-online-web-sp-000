7d4be0f8837bc296b420d25abf2affe2c09886fe
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-20 19:55:54 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,5 +42,6 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT books.title, series.title
   FROM books
   JOIN series ON books.series_id = series.id
+  JOIN authors ON series.author_id = authors.id
   "
 end
