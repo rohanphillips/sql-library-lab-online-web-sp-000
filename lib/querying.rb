ed2bd78783f406c4662a1c44f2b7e9c11785a8cf
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-20 19:11:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,7 +39,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT books.title, series.title
+  "SELECT books.title, series.title, character.name
   FROM books
   JOIN series ON series.id = books.series_id
   INNER JOIN characters ON characters.author_id = series.author_id
