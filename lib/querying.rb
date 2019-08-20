3de7982739c40d1bd96b5c53c21a4c75885f9e93
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-20 17:49:19 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,16 +20,10 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name
+  "SELECT authors.name, subgenres.name
   FROM authors
-  INNER JOIN
-    (SELECT subgenre_id FROM series
-      INNER JOIN
-        (SELECT id, name from subgenres)subgenres
-      WHERE series.subgenre_id = subgenres.id
-    )series
-  WHERE authors.id = series.author_id
-  "
+  JOIN series ON authors.id = series.author_id
+  JOIN subgenres ON series.author_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
