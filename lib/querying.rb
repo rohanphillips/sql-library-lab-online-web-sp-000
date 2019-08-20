f1a3cc5939c0d26b31a6c0f7b1c79057942237b8
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-20 18:02:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,8 @@ def select_series_title_with_most_human_characters
   (SELECT author_id, COUNT(species)
     FROM characters
     WHERE species = 'human'
-    ORDER BY COUNT(species) ASC
+    GROUP BY species
+    ORDER BY COUNT(species) DESC
   ) characters ON series.author_id = characters.author_id"
 end
 
